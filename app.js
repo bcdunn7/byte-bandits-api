ad5c05b90e0f1717644444e847e097a22f85bfee
Bryce Dunn <bryce.cc.dunn@gmail.com>
2023-06-19 21:58:53 -0500

    Work on formatting

--- app.js
+++ app.js
@@ -14,6 +14,44 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+const requestFormat = `
+{
+  gender: 'Male' | 'Female' | 'Other' | null;
+  age: number | null;
+  diet:
+    | 'Vegetarian'
+    | 'Vegan'
+    | 'Pescatarian'
+    | 'Keto'
+    | 'Paleo'
+    | 'Gluten Free'
+    | 'Latose Free'
+    | 'Tree Nut Free'
+    | 'Diabetic'
+    | 'None'
+    | null;
+  goal:
+    | 'Lose Weight'
+    | 'Gain Weight'
+    | 'Maintain Weight'
+    | 'Increase Flexibility'
+    | 'Increase Endurance'
+    | 'Practice Mindfulness'
+    | null;
+  date: string | null;
+  daysOfTheWeek: {
+    sunday: boolean;
+    monday: boolean;
+    tuesday: boolean;
+    wednesday: boolean;
+    thursday: boolean;
+    friday: boolean;
+    saturday: boolean;
+  };
+}
+`;
+const planDetailsFormat = `\`\`\`{ index: number; date: string; dayOfTheWeek: | 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday'; exercises: { name: string; sets: number; reps: number; weight: number; description: string; }[]; }[]; \`\`\``;
+
 app.use(cors());
 app.use(express.json());
 
@@ -22,27 +60,37 @@ app.listen(port, () => {
 });
 
 app.post('/', async (req, res) => {
+  console.log('Received POST request to /')
   try {
     const chatCopletion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo-0613',
+      model: 'gpt-3.5-turbo',
       messages: [
         {
           role: 'system',
-          content: 'You are an AI assistant that helps create workout plans. You will receive a list of user demographics and preferences in the format of JSON. You will create a tailored workout plan based on that information and respond in our specific JSON format.'
+          content: JSON.stringify(`You are an AI assistant that helps create workout plans. You will receive a list of user demographics and preferences in the format of JSON that adheres to this TypeScript interface: \`\`\` ${requestFormat} \`\`\`. Your goal is to create a workout plan that is tailored to the user's needs. You will respond with only a JSON object that contains the workout plan. That JSON object containing the workout plan will adhere strictly to this TypeScript interface: \`\`\` ${planDetailsFormat} \`\`\`. In your response include no other information or extra formatting. Don't lead with a greeting or any other text. And don't summarize the workout plan. Just respond with the workout plan. Make sure to include the delimeters \`\`\` in your response surrounding the JSON object representing the workout plan.`)
         },
         {
           role: 'user',
-          content: req.body.query
+          content: JSON.stringify(req.body.planDetails)
         }
       ]
     })
 
     const firstChoice = chatCopletion.data.choices[0];
 
+    console.log(firstChoice)
+
+    // const firstDelim = firstChoice.message.content.search('```');
+    // const newContent = firstChoice.message.content.slice(firstDelim + 3);
+    // const secondDelim = newContent.search('```');
+    // const finalContent = newContent.substring(0, secondDelim);
+    // console.log(finalContent);
+
     res.status(200).json({
-      resp: firstChoice
+      resp: { firstChoice }
     })
   } catch (err) {
+    console.assert('ERROR')
     console.error(err);
 
     if (err.response) {
